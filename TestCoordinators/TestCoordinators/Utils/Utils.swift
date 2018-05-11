9992d8c35c9af42f0db697853447bce9dbf6e779
Vitaly Dubov <vadubov@saritasa.com>
2018-05-11 09:37:04 +0500

    Remove unused method

--- TestCoordinators/TestCoordinators/Utils/Utils.swift
+++ TestCoordinators/TestCoordinators/Utils/Utils.swift
@@ -5,13 +5,3 @@ func delay(_ delay: Double, closure:@escaping ()->()) {
     DispatchQueue.main.asyncAfter(
         deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: closure)
 }
-
-func printFonts() {
-    let fontFamilyNames = UIFont.familyNames
-    for familyName in fontFamilyNames {
-        print("------------------------------")
-        print("Font Family Name = [\(familyName)]")
-        let names = UIFont.fontNames(forFamilyName: familyName )
-        print("Font Names = [\(names)]")
-    }
-}
